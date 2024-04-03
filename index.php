9c482648bfa65a0cf4987e667fad6827143b619c
Luigi <luigifanelli1995@libero.it>
2024-04-03 19:55:55 +0200

    esercizi 1 e 2

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// 1 esercizio
+
+//$namee = "luigi";
+//echo 'ciao a tutti sono' . $namee . "\n";
+//echo "e sono di Grumo";
+
+
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = '$text2';
+$text7 = 'bevuto';
+$text8 = "tutto";
+
+echo "$text1 $text2 $text3 $text4 $text5 $text2 $text7 $text8";
