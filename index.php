68747de88d1925e830724047214ae735bc467922
Luigi <luigifanelli1995@libero.it>
2024-04-03 20:48:40 +0200

    esercizio 3 da finire

--- index.php
+++ index.php
@@ -5,14 +5,65 @@
 //echo 'ciao a tutti sono' . $namee . "\n";
 //echo "e sono di Grumo";
 
+//esercizio 2
+//$text1 = "Marco";
+//$text2 = "hai";
+//$text3 = "sete";
+//$text4 = "?";
+//$text5 = "Perchè";
+//$text6 = '$text2';
+//$text7 = 'bevuto';
+//$text8 = "tutto";
 
-$text1 = "Marco";
-$text2 = "hai";
-$text3 = "sete";
-$text4 = "?";
-$text5 = "Perchè";
-$text6 = '$text2';
-$text7 = 'bevuto';
-$text8 = "tutto";
+//echo "$text1 $text2 $text3 $text4 $text5 $text2 $text7 $text8";
 
-echo "$text1 $text2 $text3 $text4 $text5 $text2 $text7 $text8";
+//esercizio 3
+
+$words1 = [
+    'una',
+    67,
+    'vita',
+    'colle',
+    'mi',
+    'rosso',
+  [
+    'oscura',
+    'era',
+    89,
+    [
+    'mezzo',
+    [
+      'cammin',
+      'Nel',
+      [
+        'selva',
+        'la',
+        [
+          'via',
+          'una',
+          true,
+        ]
+      ],
+    ]
+  ],
+      'ritrovai',
+      'per'
+    ],
+  'diritta'
+  ];
+
+  $words2 = [
+    'elemento1' => 25.89,
+    'elemento2' => 'nostra',
+    'elemento3' => [
+      'Virgilio',
+      'smarrita',
+      'ché'
+    ]
+  ];
+  
+
+  $parola="di";
+  $space=' ';
+  $result = $words1 [6][3][1][1] .$space .$words1 [6][3][0] .$space .$parola .$space .$words1[6][3][1][0];
+  print_r ($result);
